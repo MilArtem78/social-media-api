225775b5f645ea73ce7f80baa1d4e8ea1756e821
Artem Milchenko <pl301078map2@gmail.com>
2024-06-10 10:23:36 +0300

    Added views & serializers for handling user profiles.

--- social_media/views.py
+++ social_media/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.db.models import Count, OuterRef, Exists, Q
+from rest_framework import mixins, status
+from rest_framework.decorators import action
+from rest_framework.generics import get_object_or_404, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.viewsets import GenericViewSet
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-# Create your views here.
+from social_media.models import Profile, FollowingRelationships
+from social_media.serializers import ProfileListSerializer, ProfileSerializer
+
+
+class CurrentUserProfileView(RetrieveUpdateDestroyAPIView):
+    serializer_class = ProfileSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return (
+            Profile.objects.filter(user=self.request.user)
+            .select_related("user")
+            .prefetch_related("following", "followers")
+            .annotate(
+                followers_count=Count("followers"), following_count=Count("following")
+            )
+        )
+
+    def get_object(self):
+        return get_object_or_404(self.get_queryset())
+
+    def destroy(self, request, *args, **kwargs):
+        profile = self.get_object()
+        user = profile.user
+        response = super().destroy(request, *args, **kwargs)
+        user.delete()
+        return response
+
+
+class ProfileViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, GenericViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileListSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
