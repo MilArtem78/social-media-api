19fbf8da37d5838dcf3c25c832ad95649f2f5104
Artem Milchenko <pl301078map2@gmail.com>
2024-06-10 16:51:06 +0300

    Added filtering logic in ProfileViewSet

--- social_media/views.py
+++ social_media/views.py
@@ -42,3 +42,21 @@ class ProfileViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, GenericVi
     serializer_class = ProfileListSerializer
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = self.queryset
+
+        username = self.request.query_params.get("username")
+        first_name = self.request.query_params.get("first_name")
+        last_name = self.request.query_params.get("last_name")
+
+        if username:
+            queryset = queryset.filter(username__icontains=username)
+
+        if first_name:
+            queryset = queryset.filter(first_name__icontains=first_name)
+
+        if last_name:
+            queryset = queryset.filter(last_name__icontains=last_name)
+
+        return queryset
